b2227daae2923e898c8047e1f52b95dc3e298159
KingKidd4110 <ayiendabrian@gmail.com>
2023-04-24 03:19:39 +0300

    Documentation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,10 +3,12 @@
 
 
 def pascal_triangle(n):
+    """ Prints Pascals triangle using specs on README """
     if n <= 0:
         return []
     triangle = [[1]]
     for i in range(1, n):
+        """ Define output variables """
         row = [1]
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
