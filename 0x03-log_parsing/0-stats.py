f954fc0221a8c8f662b9ae72fdff0d7c793b3abd
KingKidd4110 <ayiendabrian@gmail.com>
2023-05-18 11:17:09 +0300

    logg

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,39 @@
 #!/usr/bin/python3
-""" interview script """
+"""script that reads stdin line by line and computes metrics"""
 import sys
-from collections import defaultdict
 
-""" possibe codes """
-status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 
-""" variables for metrics """
-total_file_size = 0
-lines_by_status_code = defaultdict(int)
-lines_processed = 0
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
 
 try:
-    """ Read lines from stdin """
     for line in sys.stdin:
-        """ Parse line using regex """
-        match = re.match
-        (r'^\S+ - \[\S+\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)$', line)
-        if match:
-            """ Extract status code and file size from match """
-            status_code = int(match.group(1))
-            file_size = int(match.group(2))
-            """ Update metrics """
-            total_file_size += file_size
-            lines_by_status_code[status_code] += 1
-            lines_processed += 1
-        else:
-            """ Skip line if it doesn't match expected format """
-            continue
+        line_in = line.split(" ")
+        if len(line_in) > 4:
+            code = line_in[-2]
+            size = int(line_in[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-        """ Print metrics every 10 lines """
-        if lines_processed % 10 == 0:
-            """ Print total file size """
-            print(f"File size: {total_file_size}")
-            """ Print lines by status code """
-            for code in status_codes:
-                if lines_by_status_code[code] > 0:
-                    print(f"{code}: {lines_by_status_code[code]}")
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
 
 
-except KeyboardInterrupt:
-    """ Handle keyboard interruption (CTRL + C) """
-    """ Print final metrics before exiting """
-    print(f"File size: {total_file_size}")
-    for code in status_codes:
-        """ print out """
-        if lines_by_status_code[code] > 0:
-            print(f"{code}: {lines_by_status_code[code]}")
+finally:
+    """ print metrics every 10 lines """
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
